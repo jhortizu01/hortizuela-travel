4d1ce8d4389480473b868b8adb57255943156a91
Janika Hortizuela <jhortizuela21@gmail.com>
2024-08-28 09:23:48 -0600

    Mobile agency nav bar

--- src/components/agencyNavBar/agencyNavBar.jsx
+++ src/components/agencyNavBar/agencyNavBar.jsx
@@ -2,15 +2,28 @@ import { NavLink } from 'react-router-dom';
 import './agencyNavBar.css';
 import logo from '../../assets/logo.png';
 import { useDispatch } from 'react-redux';
-
+import { Button, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
+import menu from '../../assets/menu.svg';
 export const AgencyNavBar = () => {
-  const dispatch = useDispatch()
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const dispatch = useDispatch();
 
   const clearCurrentUser = () => {
-    dispatch(clearCurrentUser())
-    console.log('clicked logout')
-  }
-  
+    dispatch(clearCurrentUser());
+    console.log('clicked logout');
+  };
+
   return (
     <header>
       <div className='logo-container'>
@@ -48,6 +61,63 @@ export const AgencyNavBar = () => {
           Logout
         </NavLink>
       </nav>
+      <div className='hamburger-menu'>
+        <Button
+          id='basic-button'
+          aria-controls={open ? 'basic-menu' : undefined}
+          aria-haspopup='true'
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleMenuClick}
+        >
+          <img src={menu} alt='menu' className='menu-button' />
+        </Button>
+        <Menu
+          id='basic-menu'
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleMenuClose}
+          MenuListProps={{
+            'aria-labelledby': 'basic-button',
+          }}
+        >
+          <MenuItem>
+            {' '}
+            <NavLink
+              to='/agency'
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Home
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink
+              to='/todaystrips'
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Today's Trips
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink
+              to='/finduser'
+              className={({ isActive }) =>
+                isActive ? 'active-link' : 'inactive-link'
+              }
+            >
+              Find User
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink to='/login' className='logout' onClick={clearCurrentUser}>
+              Logout
+            </NavLink>
+          </MenuItem>
+        </Menu>
+      </div>
     </header>
   );
 };
